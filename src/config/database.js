d20ecfcfb9d6435c8b521cfe80b7b19912fcf3c6
Max-Henrick <maxenrick23@gmail.com>
2023-05-03 12:06:44 -0300

    add e config do sequelize + migration de users

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'DevBurger',
+  database: 'DevBurger',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
