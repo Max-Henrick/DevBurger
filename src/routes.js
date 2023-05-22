6bed594795c03740897235d2559bdaa925d13cc0
Max-Henrick <maxenrick23@gmail.com>
2023-05-22 10:07:42 -0300

    config multer+controllerProduct+rota Upload Image

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
+import multer from 'multer'
 import UserController from './app/controllers/UserController'
 import LoginController from './app/controllers/LoginController'
+import ProductController from './app/controllers/ProductController'
+import multerConfig from './config/multer'
+
+const upload = multer(multerConfig)
 
 const routes = new Router()
 
 routes.post('/users', UserController.store)
 routes.post('/login', LoginController.store)
+routes.post('/products', upload.single('file'), ProductController.store)
 
 export default routes
