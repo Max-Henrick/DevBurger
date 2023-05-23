38beab5529f9aa9fc33f20b277b35a4790b9abc8
Max-Henrick <maxenrick23@gmail.com>
2023-05-23 09:49:55 -0300

    model_product+index/rota_get_do_product+token_jwt

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,4 +1,6 @@
 import * as Yup from 'yup'
+import jwt from 'jsonwebtoken'
+import authConfig from '../../config/auth'
 import User from '../models/User'
 
 class LoginController {
@@ -36,6 +38,9 @@ class LoginController {
       name: user.name,
       email,
       admin: user.admin,
+      token: jwt.sign({ id: user.id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
     })
   }
 }
