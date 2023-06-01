21a83e0483ff3dbd256061a341631b616e24d6f7
Max-Henrick <maxenrick23@gmail.com>
2023-06-01 10:09:28 -0300

    migration/path+update/model/controller-category

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -22,6 +22,8 @@ class CategoryController {
 
     const { name } = request.body
 
+    const { filename: path } = request.file
+
     const validateCategory = await Category.findOne({
       where: { name },
     })
@@ -30,15 +32,52 @@ class CategoryController {
       return response.status(400).json({ Error: 'Categoria já cadastrada' })
     }
 
-    const { id } = await Category.create({ name })
+    const { id } = await Category.create({ name, path })
 
-    return response.json({ id, name })
+    return response.json({ id, name, path })
   }
 
   async index(request, response) {
     const categories = await Category.findAll()
 
     return response.json(categories)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (error) {
+      return response.status(400).json({ error: error.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { name } = request.body
+
+    const { id } = request.params
+
+    const validateId = await Category.findByPk(id)
+
+    if (!validateId) {
+      return response.status(401).json()
+    }
+
+    let path
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    await Category.update({ name, path }, { where: { id } })
+
+    return response.status(200).json()
   }
 }
 
